f2ca8eeaa25ec432c8cd849d10c9f1b242077184
AAAAAZBX <1604815119@qq.com>
2026-01-16 14:45:42 +0800

    上传客户端exe文件

--- sdk/include/client.cpp
+++ sdk/include/client.cpp
@@ -1,7 +1,7 @@
 #include "client_sdk.h"
 
 int main() {
-    SMMClient::ClientSDK client("127.0.0.1", 8888);
+    SMMClient::ClientSDK client("192.168.20.31", 8888);
     client.StartInteractiveCLI(); // 启动交互式命令行
     return 0;
 }
